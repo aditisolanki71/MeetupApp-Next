50bd02f33c455b384cab7b9379542ecb595a456a
Unknown <adtsolanki22@gmail.com>
2022-09-18 18:34:28 +0530

    sending a request to the api route

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@
 import { MongoClient } from "mongodb";
      
 export async function connectToDatabase() {
-   const MONGO_URI = `mongodb+srv://aditi:NruNqUTV1OPMxTvN@cluster0.v364j.mongodb.net/authenticationApp?retryWrites=true&w=majority`;
+   const MONGO_URI = `mongodb+srv://aditi:NruNqUTV1OPMxTvN@cluster0.v364j.mongodb.net/meetupApp?retryWrites=true&w=majority`;
    const client = await MongoClient.connect(MONGO_URI);
    return client;
 }
