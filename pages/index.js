bd6a4680047a9364c357e134de042808566848ed
Unknown <adtsolanki22@gmail.com>
2022-09-18 20:18:10 +0530

    Added head metadata to pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
+import Head from 'next/head';
 import MeetupList from "../components/meetups/MeetupList";
 import { connectToDatabase } from "../helper/db";
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
         <MeetupList meetups={props.meetups}/>
     </Fragment>
   )
